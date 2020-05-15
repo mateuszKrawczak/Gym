d31754181d192a2eca0f2bbba2ec60a0b7e05eb9
KarolJaskolka <jaskolka.karol@gmail.com>
2020-05-15 23:11:19 +0200

    Add Reception class

---
+++ src/Reception.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Reception {
+
+    private ArrayList<Bodybuilder> people;
+    private JTextArea textArea;
+
+    public Reception(JTextArea textArea){
+        this.textArea = textArea;
+        this.people = new ArrayList<>();
+    }
+
+    public synchronized void comeToReception(Bodybuilder bodybuilder){
+        this.people.add(bodybuilder);
+        this.setTextArea();
+    }
+
+    public synchronized void customerService(){
+        if(this.people.size() > 0){
+            Bodybuilder bodybuilder = this.people.get(0);
+            bodybuilder.setKeyOwnership();
+            if(bodybuilder.getKeyOwnership()){
+                bodybuilder.collectLockerKey();
+            }
+            else{
+                bodybuilder.returnLockerKey();
+            }
+            this.people.remove(bodybuilder);
+            this.setTextArea();
+        }
+    }
+
+    public synchronized void setTextArea(){
+        textArea.setText("");
+        if(people.size() > 0){
+            people.forEach(person -> {
+                textArea.append(person.toString() + "\n");
+            });
+        }
+        else textArea.setText("Free");
+    }
+
+
+}
