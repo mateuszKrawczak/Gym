10dfa409008c58f2755c55e57c1d59897b7fe39e
KarolJaskolka <jaskolka.karol@gmail.com>
2020-05-15 23:10:59 +0200

    Add ChangingRoom class

---
+++ src/ChangingRoom.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class ChangingRoom {
+
+    private int capacity;
+    private ArrayList<Bodybuilder> people;
+    private JTextArea textArea;
+
+    public ChangingRoom(int capacity, JTextArea textArea){
+        this.capacity = capacity;
+        this.people = new ArrayList<>(capacity);
+        this.textArea = textArea;
+    }
+
+    public boolean isAvailable() {
+        if (people.size() < capacity) {
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized void useChangingRoom(Bodybuilder bodybuilder){
+        this.people.add(bodybuilder);
+        this.setTextArea();
+        System.out.println(this.people.size());
+    }
+
+    public synchronized void exitChangingRoom(Bodybuilder bodybuilder){
+        this.people.remove(bodybuilder);
+        this.setTextArea();
+        System.out.println(this.people.size());
+    }
+
+    public synchronized void setTextArea(){
+        textArea.setText("");
+        if(people.size() > 0){
+            people.forEach(person -> {
+                textArea.append(person.toString() + "\n");
+            });
+        }
+        else textArea.setText("Free");
+    }
+
+}
